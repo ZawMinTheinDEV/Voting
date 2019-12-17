01a3940bed705bec677e572901c8ce421277e828
Zaw-Min-Thein <zawminthein@ucsm.edu.mm.com>
2019-12-17 09:55:39 +0630

    17.12.2019/10am

--- backend/function.js
+++ backend/function.js
@@ -1,26 +1,49 @@
 var source = require('./source');
 var con = source.dbConnection;
 
-vote('002Ou1zQ', 1, 1);
+
+
+//UPDATE voter V ,participant p   SET p.vote_count =p.vote_count-1 WHERE p.cid=1 AND voter.code='002Ou1zQ'
+//deleteVote("002Ou1zQ", '1')
+function deleteVote(code, cid) {
+    //check if the code is already used
+    var decrese = 'UPDATE voter V ,participant p   SET p.vote_count =p.vote_count-1 WHERE p.pid=v.' + cid + ' AND v.code=?';
+    //console.log(check);
+    con.query(decrese, [code], function (err, results) {
+        if (err) throw err;
+
+
+
+        // mark the code so it can't be used again
+        var mark = 'UPDATE voter SET voter.' + cid + '="active" where voter.code=?;';
+        con.query(mark, [code],function (err, results) {
+            if (err) throw err;
+            console.log("marked");
+        });
+
+    });
+}
+
+//vote('002Ou1zQ', 1, 1);
 function vote(code, cid, pid) {
 
     //check if the code is already used
-    var check = 'select voter.' + cid + ' from voter where code="' + code + '" and voter.' + cid + '="active"';
-    console.log(check);
-    con.query(check, function (err, results) {
+    var check = 'select voter.' + cid + ' from voter where code=?  and voter.' + cid + '="active"';
+    //console.log(check);
+    con.query(check, [code], function (err, results) {
         if (err) throw err;
         if (results.length > 0) {
 
             //increse vote_count for a specific code
-            var increse = 'UPDATE participant P SET p.vote_count =p.vote_count+1 WHERE p.pid=' + pid + ';';
+            var increse = 'UPDATE participant P SET p.vote_count =p.vote_count+1 WHERE p.pid=?;';
 
-            con.query(increse, function (err, results) {
+            con.query(increse, [pid], function (err, results) {
                 if (err) throw err;
                 console.log("incremented");
             });
             // mark the code so it can't be used again
-            var mark = 'UPDATE voter SET voter.' + cid + '=' + pid + ' where voter.code="' + code + '";';
-            con.query(mark, function (err, results) {
+            var mark = 'UPDATE voter SET voter.' + cid + '=? where voter.code=?;';
+            con.query(mark, [pid, code], function (err, results) {
                 if (err) throw err;
                 console.log("marked");
             });
